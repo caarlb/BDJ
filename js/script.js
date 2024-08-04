ab5fce1f77ea3ead299d014133f6e85eda601aed
caarlb <s.caarlb@gmail.com>
2024-08-04 02:04:57 -0600

    encabezado con imagen de fondo listo, y musica de fondo lista

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ if (confirmButton) {
 } else {
     console.error('El botón confirmButton no se encontró en el DOM.');
 }
+
+
+const miMusica = document.getElementById('miMusica');
+const botonMusica = document.getElementById('botonMusica');
+
+botonMusica.addEventListener('click', () => {
+    if (miMusica.paused) {
+        miMusica.play();
+        botonMusica.textContent = '||';
+    } else {
+        miMusica.pause();
+        botonMusica.textContent = '▶';
+    }
+});
+
